6eebdf29171bd41a0d927e06280d06e5958a9e53
Kim Seonghyeon <kshshkim@gmail.com>
2022-05-16 14:11:56 +0900

    생성자 추가

--- src/chooldong/window/Welcome.java
+++ src/chooldong/window/Welcome.java
@@ -1,25 +1,44 @@
 package chooldong.window;
 import chooldong.frame.ChooldongFrame;
+import chooldong.request.AbstractAuthRequest;
+import chooldong.request.AbstractDataRequest;
+import chooldong.request.MockAuth;
+import chooldong.request.MockData;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+
 
 public class Welcome extends ChooldongFrame {
-    public Welcome() {
-        setDefault();
+    AbstractAuthRequest authRequest;
+    AbstractDataRequest dataRequest;
+
+    public Welcome() {  // 인자 없이 생성
+        dataRequest = new MockData();
+        authRequest = new MockAuth();
+        init();
+    }
+
+    public Welcome(AbstractDataRequest dataRequest, AbstractAuthRequest authRequest) {  // 요청 객체 지정
+        this.authRequest = authRequest;
+        this.dataRequest = dataRequest;
+        init();
+    }
+
+    public void init() {
+        setDefault();  // 상속됨
         setTitle("출동");
-        this.c.addMouseListener(new MouseAdapter() {
+        this.c.addMouseListener(new MouseAdapter() {  // 마우스 이벤트 리스너
             @Override
-            public void mouseClicked(MouseEvent e) {
-                StudentOrTeacher sot = new StudentOrTeacher();
-                sot.showWindow();
-                setVisible(false);
+            public void mouseClicked(MouseEvent e) {  // 마우스 클릭시 실행
+                StudentOrTeacher sot = new StudentOrTeacher(dataRequest, authRequest);
+                sot.showWindow();  // sot.setVisible(true)
+                dispose();  // 창 닫음
             }
         });
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // 테스트용
         Welcome wc = new Welcome();
         wc.showWindow();
     }
