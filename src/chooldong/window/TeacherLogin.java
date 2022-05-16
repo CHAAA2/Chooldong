63fbfd535f6f5d09dc1ad91a002ea85cb564ade9
Kim Seonghyeon <kshshkim@gmail.com>
2022-05-16 13:05:05 +0900

    학습자 교수자 로그인 창

---
+++ src/chooldong/window/TeacherLogin.java
@@ -0,0 +1,47 @@
+package chooldong.window;
+
+import chooldong.frame.LogIn;
+import chooldong.request.AbstractAuthRequest;
+import chooldong.request.AbstractDataRequest;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TeacherLogin extends LogIn {
+    public void tlInit() {
+        JButton gen = new JButton("생성");
+        gen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onGenBtnPressed();
+            }
+        });
+        super.c.add(gen);
+
+    }
+    public TeacherLogin(AbstractDataRequest dataRequest, AbstractAuthRequest authRequest) {
+        this.dataRequest = dataRequest;
+        this.authRequest = authRequest;
+        tlInit();
+    }
+
+    public TeacherLogin(){
+        tlInit();
+    }
+
+    @Override
+    public void onViewBtnPressed() {
+        super.onViewBtnPressed();
+    }
+
+    public void onGenBtnPressed() {
+        System.out.println(this.idField.getText());
+        System.out.println(this.pwField.getPassword());
+    }
+
+    public static void main(String[] args) {
+        TeacherLogin tl = new TeacherLogin();
+        tl.showWindow();
+    }
+}
