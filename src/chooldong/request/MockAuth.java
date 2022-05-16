de2aca1cd39268c4e652942350e503e4ff7ac867
Kim Seonghyeon <kshshkim@gmail.com>
2022-05-16 14:08:53 +0900

    타입 에러 수정

--- src/chooldong/request/MockAuth.java
+++ src/chooldong/request/MockAuth.java
@@ -1,11 +1,12 @@
 package chooldong.request;
 
+import java.util.Arrays;
 import java.util.Objects;
 
-public class MockAuth extends AbstractAuthRequest {
+public class MockAuth extends AbstractAuthRequest {  // 인증 요청 mock 클래스
     @Override
     public String getToken(String id, char[] pw) {
-        if (Objects.equals(id, "myid") && Objects.equals(pw, "passwd")) {
+        if (Objects.equals(id, "myid") && Objects.equals(Arrays.toString(pw), "passwd")) {
             return "my_token";
         }
         else {
