d7d5320493b5208062e6bad538ca0ac0883ed106
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2022-05-23 10:44:32 +0900

    minor change

--- src/chooldong/Main.java
+++ src/chooldong/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Welcome wc = new Welcome();
         wc.showWindow();
-        String asdf = System.getProperty("user.dir");
-        System.out.println(asdf);
     }
 }
 
